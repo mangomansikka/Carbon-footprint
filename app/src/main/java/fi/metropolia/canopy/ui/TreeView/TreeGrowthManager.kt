68184547973458b5bad44020f7236023c8b57568
yasminbx <122457280+yasminbx@users.noreply.github.com>
2026-05-04 20:22:03 +0300

    tree fixes and updated values

--- app/src/main/java/fi/metropolia/canopy/ui/TreeView/TreeGrowthManager.kt
+++ app/src/main/java/fi/metropolia/canopy/ui/TreeView/TreeGrowthManager.kt
@@ -1,19 +1,28 @@
 package fi.metropolia.canopy.ui.treeview
 
 enum class TreeStage {
-    SEED, SPROUT, SMALL_TREE, BIG_TREE, FULL_TREE, DESTROYED
+    SEED,           // 0 - 100
+    SPROUT,         // 100 - 220
+    SMALL_TREE,     // 220 - 330
+    MEDIUM_TREE,    // 330 - 440
+    BIG_TREE,       // 440 - 550
+    FULL_TREE,      // 550 - 660 (Terve ja täysikasvuinen)
+    SICK,           // 660 - 800 (Huonokuntoinen)
+    DEAD            // 800+ (Kuollut)
 }
 
 object TreeGrowthManager {
 
     fun getTreeStage(monthlyEmissionKg: Double): TreeStage {
         return when {
-            monthlyEmissionKg < 30.0  -> TreeStage.FULL_TREE   // Erinomainen (Kävely, pyöräily, juna, metro)
-            monthlyEmissionKg < 80.0  -> TreeStage.BIG_TREE    // Hyvä
-            monthlyEmissionKg < 150.0 -> TreeStage.SMALL_TREE  // Kohtuullinen
-            monthlyEmissionKg < 300.0 -> TreeStage.SPROUT      // Melko korkea
-            monthlyEmissionKg < 1000.0 -> TreeStage.DESTROYED        // Korkea (Paljon autoilua)
-            else -> TreeStage.DESTROYED                       // Erittäin korkea (Kriittinen)
+            monthlyEmissionKg < 100.0 -> TreeStage.SEED
+            monthlyEmissionKg < 220.0 -> TreeStage.SPROUT
+            monthlyEmissionKg < 330.0 -> TreeStage.SMALL_TREE
+            monthlyEmissionKg < 440.0 -> TreeStage.MEDIUM_TREE
+            monthlyEmissionKg < 550.0 -> TreeStage.BIG_TREE
+            monthlyEmissionKg < 660.0 -> TreeStage.FULL_TREE
+            monthlyEmissionKg < 800.0 -> TreeStage.SICK
+            else -> TreeStage.DEAD
         }
     }
 }
